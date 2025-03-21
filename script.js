885b3ed5b3e45553d8c7b2b626942d920b1c44d7
Tom Wood <tomwood1875@gmail.com>
2025-03-21 13:32:04 +0000

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const quotesList = 
+[
+    "Spend each day trying to be a little wiser than you were when you woke up. Discharge your duties faithfully and well. Systematically you get ahead, but not necessarily in fast spurts. Nevertheless, you build discipline by preparing for fast spurts. Slug it out one inch at a time, day by day. At the end of the day – if you live long enough – most people get what they deserve.",
+    "Neither Warren nor I are smart enough to make decisions with no time to think. We make actual decisions very rapidly, but that’s because we have spent so much time preparing ourselves by quietly reading.",
+    "If you skillfully follow the multidisciplinary path, you will never wish to come back. It would be like cutting off your hands."
+]
+
+function generateQuote()
+{
+    document.getElementById("test").innerHTML = quotesList[2];
+}
+
+// store quotes (for now) in a list or array and use a rand function to display array[3] etc
+
